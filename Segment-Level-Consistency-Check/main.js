5241884498d991a270dee04342f4f1c4be9193a1
EirikBirkeland <birketrans@gmail.com>
2015-11-07 15:32:13 +0100

    refactor

--- Segment-Level-Consistency-Check/main.js
+++ Segment-Level-Consistency-Check/main.js
@@ -5,43 +5,64 @@ Array.prototype.isAoA = function() {
   else return false;
 }
 
+Array.prototype.anyMatch = function(arr2) {
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (this[i] == arr2[j]) return true;
+    }
+  }
+  return false;
+}
+
 var sArr = ["cat", "dog", "dog", "dog", "dog", "fish", "monkey", "monkey", "chair", "stool"];
 var tArr = ["katt", "hund", "bikkje", "sjefer", "puddel", "fish", "ape", "apekatt", "stol", "stol"];
 
-console.log("Final output is " + findInconsistent(sArr, tArr));
+console.log("Final output is :");
+var result = findInconsistent(sArr, tArr);
+for(let i=0;i<result.length;i++){
+  console.log("Indices " + result[i] + " are inconsistently translated");
+}
+
+var result = findInconsistent(tArr, sArr);
+for(let i=0;i<result.length;i++){
+  console.log("Indices " + result[i] + " are translated the same way, but source segments are different");
+}
 
 function findInconsistent(arr1, arr2) {
-    var obj = {};
-    // Find inconsistent translations in source/target or target/source
-    for (var i = 0; i < arr1.length; i++) {
-      for (var j = 0; j < arr1.length; j++) {
-        // not same item && strlen same && identical text content
-        if ((i != j) && (arr1[i].length == arr1[j].length) && (arr1[i] == arr1[j])) {
-          if (arr2[i] != arr2[j]) {
-            if (i <= j) {
-              console.log(`${arr2[i]} (index ${i}) is different from ${arr2[j]} (index ${j})`);
-              obj[i] = j;
-            }
+  // Find inconsistent translations in source/target or target/source
+  var obj = generateObj(arr1, arr2);
+  var outArr = [];
+  outArr = objToArr(obj, outArr);
+  outArr = mergeArrays(outArr);
+  outArr = outArr.isAoA() ? outArr.map(a => arrayUnique(a).sort()) : arrayUnique(outArr).sort();
+  return outArr;
+}
+
+function generateObj(arr1, arr2) {
+  var obj = {};
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr1.length; j++) {
+      // not same item && strlen same && identical text content
+      if ((i != j) && (arr1[i].length == arr1[j].length) && (arr1[i] == arr1[j])) {
+        if (arr2[i] != arr2[j]) {
+          if (i <= j) {
+            // console.log(`${arr2[i]} (index ${i}) is different from ${arr2[j]} (index ${j})`);
+            obj[i] = j;
           }
         }
       }
     }
-    console.log("The object's contents are: ");
-    console.log(obj);
-
-    var outArr = [];
-
-    for (let key of Object.keys(obj)) {
-      outArr.push([key, String(obj[key])]);
-    }
-    outArr = mergeArrays(outArr);
-    //  outArr = outArr.isAoA() ? outArr.map(a => arrayUnique(a)) : arrayUnique(outArr);
-    outArr = outArr.map(a => arrayUnique(a));
-    return outArr;
   }
-  // set aside for now
+  return obj;
+}
 
-// GOOD
+function objToArr(obj, arr) {
+  for (let key of Object.keys(obj)) {
+    arr.push([key, String(obj[key])]);
+  }
+  return arr;
+}
+
 function arrayUnique(a) { // IN ["12", "12"]
     return a.reduce(function(p, c) {
       if (p.indexOf(c) < 0) p.push(c);
@@ -49,26 +70,15 @@ function arrayUnique(a) { // IN ["12", "12"]
     }, []);
   } // OUT ["12"]
 
-// GOOD
 function mergeArrays(AoA) { // IN ["12", "21", "34"] OUT ["1221", "34"]
   if (!AoA.isAoA()) throw (AoA + " is not an array!");
-  //AoA is [[1,2], [2,3], [1,4]]
   for (let i = 0; i < AoA.length; i++) {
     for (let j = 0; j < AoA.length; j++) {
-      if (i != j && anyMatch(AoA[i], AoA[j])) {
-        AoA[i] = AoA[i].concat(AoA[j]); // ok
-        AoA.splice(j, 1); // ok
+      if (i != j && AoA[i].anyMatch(AoA[j])) {
+        AoA[i] = AoA[i].concat(AoA[j]);
+        AoA.splice(j, 1);
       }
     }
   }
-  // AoA = arrayUnique(AoA); //Output should be [[1,2,3,4]]
   return AoA;
-
-  function anyMatch(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-      for (let j = 0; j < arr2.length; j++) {
-        if (arr1[i] == arr2[j]) return true;
-      }
-    }
-    return false;
-  }
+}
